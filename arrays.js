7bfffafc9c6510fbfe403c8757b805b919aadf32
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-23 11:47:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,9 +17,12 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray (array, element) {
-  
+  const arr = array
+  arr.push(element)
+  return arr
 }
 
 function destructivelyAddElementToEndOfArray (array, element) {
-  
+  array[array.length] = element
+  return array
 }
