9afb1514d5bc1ee4afa66161cb88f8a9dcec161f
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-23 11:49:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,8 @@ function destructivelyAddElementToEndOfArray (array, element) {
 }
 
 function accessElementInArray(array, index) {
-  return array[index]
+  const element = array[index]
+  return element
 }
 
 
